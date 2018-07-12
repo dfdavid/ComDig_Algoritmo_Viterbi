0c4f275e8f7c4f28ba3fc9e3e98d495a5b05f539
dfdavid <david.do.it@gmail.com>
2018-07-12 18:23:18 -0300

    1 commit
    1er commit, viterbi no funciona todavia

---
+++ algoritmoViterbi.m
@@ -0,0 +1,38 @@
+clear all 
+close all
+
+%% VARIABLES NECESARIAS
+tamVentana=7; %es igual a la profundidad de truncamiento
+Estados=[-1 -1;
+         -1  1;
+          1 -1;
+          1  1] % Esta matriz guarda los estados posibles del trellis
+
+%% STATE_MATRIX
+% R = randi(IMAX,N) returns an N-by-N matrix containing pseudorandom
+%     integer values drawn from the discrete uniform distribution on 1:IMAX.
+%     randi(IMAX,M,N) or randi(IMAX,[M,N]) returns an M-by-N matrix.
+
+% state_matrix=randi(4,4,7);
+state_matrix=zeros(4,1); %aca se inicializa la matriz de estados
+
+%% COST_MATRIX 
+%esta matriz guarda el costo acumulado en cada estado, no es necesario que
+%se guarde el historico de los costos de estados. Se puede implementar como
+%un vector: [cantEstados,1]
+
+% cost_matrix=randi(20,4,7); 
+cost_matrix=zeros(4,1); %inicializo la matriz de costos
+
+
+%% TRELLIS provisto como matriz
+
+
+
+%si la state_matrix esta llena se llama a traceback
+if length(state_matrix)>=tamVentana
+    simbolo=traceback(state_matrix,cost_matrix,tamVentana,Estados);
+end
+disp('el simbolo decodificado es: ')
+disp(simbolo)
+
---
+++ traceback.m
@@ -0,0 +1,32 @@
+function s=traceback(state_matrix,cost_matrix, tamVentana,Estados)
+    dimensiones=size(state_matrix);
+    nEstados=dimensiones(1);
+%     nIteraciones=dimensiones(2);
+    %determino en la ultima columna de state_matrix cual es el estado
+    %inicial 
+    for e=1:nEstados 
+        %max=0; si le asigno cero aca siempre me pisa el valor de max de la
+        %pasada anterior
+        if e==1 %solo entro en la primera pasada para inicializar
+            max=0;
+        end
+        if (cost_matrix(e,tamVentana)>max )
+            max=cost_matrix(e,tamVentana);
+            eInicial=e;
+        end
+    end
+        
+        %una vez conocido el estado inicial comienza el traceback
+        for j=tamVentana:-1:1
+            eAnterior=state_matrix(eInicial,j);
+            simbolo=Estados(eInicial,1); %%Estados es una matriz cuyas filas son los estados posible y las columnas on las componentes de dichos estados
+            if j==1
+                s=simbolo;
+            end
+            eInicial=eAnterior;
+        end
+end
+                
+        
+        
+% end 
