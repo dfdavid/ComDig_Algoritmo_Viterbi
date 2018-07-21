a1dc4c3c8be59ce739235c8d43a14b3a393c9c87
dfdavid <david.do.it@gmail.com>
2018-07-20 21:07:08 -0300

    viterbi 3.0
    -Se trabaja con state matrix en estado de regimen omitiendo el transitorio previo.
    
    -se escribio la función shift (para state matrix).
    
    -se esta trabajando en el nuevo bloque que llena la ultima columna de state matrix en regimen.

--- algoritmoViterbi.m
+++ algoritmoViterbi.m
@@ -49,42 +49,43 @@ cost_vector=zeros(4,1); %inicializo el vector de costos
 dimension=size(y_matrix);
 cantFilas=dimension(1); %cantidad de tuplas que se recibieron
 
-for j=1:cantFilas %aca inicio el recorrido por las tuplas
+for j=1:tamVentana %aca inicio el recorrido por las tuplas
     
     %si el sistema esta iniciando, se asume que parte del estado 1 de la
     %primera iteracion del Trellis
     
-    if j==1 %si se trata de la primera tupla, asumo que el sistema se inicia en el estado 1
-
-            costA=y_matrix(j,(1:2))*Trellis(1,(5:6))'; %este es el producto punto entre la tupla recibida y el peso de la rama
-            costB=y_matrix(j,:)*Trellis(1,[7:8])';
-            cost_vector(1)=max(costA,costB);
-            state_matrix(1,1)=1; % al inicio no existe otra psibilidad para el estado previo por eso es =1
-
-    elseif j==2
-            for e2=1:2 % en la segunda tupla recibida despues de un reset solo podran ser alcanzados los dos primeros estados
-                costA=y_matrix(j,:)*Trellis(e2,(5:6))';
-                costB=y_matrix(j,:)*Trellis(e2,(7:8))';
-                %en el siguiente bloque 'if' se determina:
-                % -la mayor metrica de estado (la guardo en el cost vector) y,
-                % -cual es el estado de procedencia (lo guardo en la state_matrix)
-                if costA > costB
-                    cost_vector(e2)=cost_vector(e2)+ costA;
-                    state_matrix(e2,j)= Trellis(e2,2);
-                else
-                    %cost_vector(e2)=cost_vector(e2)+ costB;
-                    %state_matrix(e2,j)= Trellis(e2,3); en la segunda
-                    %iteracion, es decir j=2, si estoy en el S2 solo puedo
-                    %provenir del S1
-                    state_matrix(e2,j)=1; %forzosamente el estado previo es S1
-                end
-            end
-    else
+%     if j==1 %si se trata de la primera tupla, asumo que el sistema se inicia en el estado 1
+% 
+%             costA=y_matrix(j,(1:2))*Trellis(1,(5:6))'; %este es el producto punto entre la tupla recibida y el peso de la rama
+%             costB=y_matrix(j,:)*Trellis(1,[7:8])';
+%             cost_vector(1)=max(costA,costB);
+%             state_matrix(1,1)=1; % al inicio no existe otra psibilidad para el estado previo por eso es =1
+% 
+%     elseif j==2
+%             for e2=1:2 % en la segunda tupla recibida despues de un reset solo podran ser alcanzados los dos primeros estados
+%                 costA=y_matrix(j,:)*Trellis(e2,(5:6))';
+%                 costB=y_matrix(j,:)*Trellis(e2,(7:8))';
+%                 %en el siguiente bloque 'if' se determina:
+%                 % -la mayor metrica de estado (la guardo en el cost vector) y,
+%                 % -cual es el estado de procedencia (lo guardo en la state_matrix)
+%                 if costA > costB
+%                     cost_vector(e2)=cost_vector(e2)+ costA;
+%                     state_matrix(e2,j)= Trellis(e2,2);
+%                 else
+%                     %cost_vector(e2)=cost_vector(e2)+ costB;
+%                     %state_matrix(e2,j)= Trellis(e2,3); en la segunda
+%                     %iteracion, es decir j=2, si estoy en el S2 solo puedo
+%                     %provenir del S1
+%                     state_matrix(e2,j)=1; %forzosamente el estado previo es S1
+%                 end
+%             end
+%     else
       %cuando el sistema entra en regimen, en este caso para j=3 en adelante
       %se recorren todos los estados para calcular la metrica de estado
-	    for e=1:length(Estados)
-	           costA=y_matrix(j)*Trellis(e2,(5:6));
-	           costB=y_matrix(j)*Trellis(e2,(7:8));
+	    
+      for e=1:length(Estados) %
+	           costA=y_matrix(j,:)*Trellis(e,(5:6))';
+	           costB=y_matrix(j,:)*Trellis(e,(7:8))';
 	           
 	           if costA > costB
 	               cost_vector(e)=cost_vector(e)+ costA;
@@ -94,16 +95,33 @@ for j=1:cantFilas %aca inicio el recorrido por las tuplas
 	               state_matrix(e,j)=Trellis(e,3);
 	           end
     	end
-    end
+    
         
         if j>=tamVentana
             simbolo_decodificado=traceback(state_matrix, cost_vector, tamVentana, Estados);
-            disp('el simbolo decodificado es: ')
-            disp(simbolo_decodificado)
+            disp('el simbolo decodificado es: ') % esto podria estar dentro de la funcion TRACEBACK
+            disp(simbolo_decodificado)           %   
             
-            %shift state_matrix
-            state_matrix=[state_matrix(:,(2:tamVentana),zeros(length(Estados),1))];
+            %shift state_matrix     //ESTO PODRIA/DEBERIA SER UNA
+            %FUNCION,ya que se va a utilizar mas adelante tambien y se
+            %repetiria codigo
+            %state_matrix=state_matrix(:,(2:tamVentana));
+            %state_matrix(:,3)=zeros(length(Estados),1);
+            state_matrix=shift(state_matrix);
+            
         end
         
+end
+
+%j=tamVentana+1;      no incremento 'j' aca porque lo hago dentro del ciclo
+                      %while
+ultimaCol=tamVentana;
+while j<length(y_matrix)
+   j=j+1;
+   %llamar al llenador de matrices o escribirlo aqui
+   
+   %llamar a traceback
+   
+   %llamar a shift
 end
 
